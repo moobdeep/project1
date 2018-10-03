295c636335eeb30282c40ecbdc16f88b6949aec8
Vasyl Moroz <moobdeep@bigmir.net>
2018-10-03 11:39:29 +0300

    update import.py

--- import.py
+++ import.py
@@ -1,12 +1,16 @@
 import csv
 import os
 
+from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+app = Flask(__name__)
 
+os.environ['DATABASE_URL'] = 'postgres://vwkbfwdszgpuhl:5af21389486c95e925e6103b2fdc61becd23178e9ffd938a79575eaad369bbdc@ec2-54-217-235-137.eu-west-1.compute.amazonaws.com:5432/d49ua05gin9msl'
+
+engine = create_engine('postgres://vwkbfwdszgpuhl:5af21389486c95e925e6103b2fdc61becd23178e9ffd938a79575eaad369bbdc@ec2-54-217-235-137.eu-west-1.compute.amazonaws.com:5432/d49ua05gin9msl')
+db = scoped_session(sessionmaker(bind=engine))
 
 def main():
     f = open("books.csv")
